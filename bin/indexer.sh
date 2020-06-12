1960a34529a28c460850c18842cfa394ae858261
swsong <songaal@gmail.com>
2020-06-12 11:36:58 +0900

    로그문제해결.

--- bin/indexer.sh
+++ bin/indexer.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-java -classpath indexer-1.1.0.jar:Altibase.jar:danawa-search-1.0.0.jar org.springframework.boot.loader.JarLauncher
+trap '' 1 2
+PORT=8080
+java -classpath indexer-1.1.0.jar:Altibase.jar:danawa-search-1.0.0-jar-with-dependencies.jar org.springframework.boot.loader.JarLauncher --server.port=$PORT >> output.log 2>&1 &
+
--- src/main/java/com/danawa/fastcatx/indexer/IndexerApplication.java
+++ src/main/java/com/danawa/fastcatx/indexer/IndexerApplication.java
@@ -1,13 +1,23 @@
 package com.danawa.fastcatx.indexer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IndexerApplication {
+public class IndexerApplication implements CommandLineRunner {
+
+    private static Logger logger = LoggerFactory.getLogger(IndexerApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(IndexerApplication.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        logger.info("Indexer Application을 시작합니다!");
     }
 
 }
