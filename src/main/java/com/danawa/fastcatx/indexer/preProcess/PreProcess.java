b18d3d798e51ec685231b91d4f53afdb755db102
joonwoo <jwk@danawa.com>
2021-06-14 17:25:19 +0900

    .

--- src/main/java/com/danawa/fastcatx/indexer/preProcess/PreProcess.java
+++ src/main/java/com/danawa/fastcatx/indexer/preProcess/PreProcess.java
@@ -10,13 +10,9 @@ import java.util.Map;
 public interface PreProcess {
     enum TYPE { NTOUR, CATEGORY_KEYWORD, VM_KEYWORD, VM_FIRSTMAKE_DATE }
 
-    default void starter(Job job) throws SQLException, ClassNotFoundException {
+    default void starter(Job job) throws Exception {
         Map<String, Object> payload = job.getRequest();
         String type = (String) payload.getOrDefault("type", "");
-        String dataSQL = (String) payload.get("dataSQL");
-        String env = (String) payload.get("env");
-
-
         if (TYPE.NTOUR.name().equalsIgnoreCase(type)) {
             new NTourPreProcess(job).start();
         } else if (TYPE.CATEGORY_KEYWORD.name().equalsIgnoreCase(type)) {
@@ -24,16 +20,11 @@ public interface PreProcess {
         } else if (TYPE.VM_KEYWORD.name().equalsIgnoreCase(type)) {
 
         } else if (TYPE.VM_FIRSTMAKE_DATE.name().equalsIgnoreCase(type)) {
-            String alti_master_url = (String) payload.get("alti_master_url");
-            String alti_slave_url = (String) payload.getOrDefault("alti_slave_url",null);
-            String alti_rescue_url = (String) payload.getOrDefault("alti_rescue_url",null);
-            String alti_user = (String) payload.get("alti_user");
-            String alti_password = (String) payload.get("alti_password");
-            new VmFirstMakeDatePreProcess(job, dataSQL, env, alti_master_url, alti_slave_url, alti_rescue_url, alti_user, alti_password);
+            new VmFirstMakeDatePreProcess(job).start();
         }
     }
 
-    void start();
+    void start() throws Exception;
 
     class EmptyPreProcess implements PreProcess {
         private static final Logger logger = LoggerFactory.getLogger(EmptyPreProcess.class);
--- src/test/java/com/danawa/fastcatx/indexer/preProcess/PreProcessTest.java
+++ src/test/java/com/danawa/fastcatx/indexer/preProcess/PreProcessTest.java
@@ -63,5 +63,9 @@ public class PreProcessTest {
 
     }
 
+    @Test
+    public void cronTest() {
+
+    }
 
 }
