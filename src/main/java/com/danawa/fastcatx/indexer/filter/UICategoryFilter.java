10f7cc5f028e210a311ed5e0f4505fe63fe7407a
joonwoo <jwk@danawa.com>
2021-07-22 14:13:26 +0900

    .

--- src/main/java/com/danawa/fastcatx/indexer/filter/UICategoryFilter.java
+++ src/main/java/com/danawa/fastcatx/indexer/filter/UICategoryFilter.java
@@ -4,10 +4,13 @@ import com.danawa.convertcategory.CategoryScheduler;
 import com.danawa.convertcategory.entity.CategoryMappingModel;
 import com.danawa.convertcategory.entity.UICategory;
 import com.danawa.fastcatx.indexer.Filter;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestTemplate;
 
+import java.lang.reflect.Type;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -19,18 +22,18 @@ public class UICategoryFilter implements Filter {
     @Override
     public void init(Map<String, Object> payload) {
         String categoryMappingUrl = (String) payload.getOrDefault("categoryMappingUrl", "");
-        logger.info("UI카테고리 조회 URL: {}", categoryMappingUrl);
-        CategoryMappingModel mappingModel = restTemplate.getForObject(categoryMappingUrl, CategoryMappingModel.class);
-        categoryScheduler = new CategoryScheduler(mappingModel);
+        logger.info("UI카테고리 맵핑 조회 URL: {}", categoryMappingUrl);
+        String body = restTemplate.getForObject(categoryMappingUrl, String.class);
+        Type type = new TypeToken<CategoryMappingModel<String, List<CategoryMappingModel.Mapping>>>(){}.getType();
+        CategoryMappingModel<String, List<CategoryMappingModel.Mapping>> mappingModel = new Gson().fromJson(body, type);
+        if (mappingModel != null) {
+            logger.info("물리카테고리 갯수: {}", mappingModel.keySet().size());
+            categoryScheduler = new CategoryScheduler(mappingModel);
+        }
     }
 
     @Override
     public Map<String, Object> filter(Map<String, Object> item) {
-        // 물리카테고리 코드
-        List<Integer> categoryCode = new ArrayList<>();
-        for (int i = 4; i >= 1; i--) {
-            categoryCode.add(Integer.parseInt(item.getOrDefault("categoryCode" + i, "0").toString()));
-        }
         // 옵션들
         String makerCodeStr = (String) item.getOrDefault("makerCode", null);
         String brandCodeStr = (String) item.getOrDefault("brandCode", null);
@@ -38,16 +41,30 @@ public class UICategoryFilter implements Filter {
         Integer brandCode = brandCodeStr == null ? null : Integer.parseInt(brandCodeStr);
 
         // attr 123,123,123
-        List<String> nAttributeValueSeqStrList = Arrays.asList(((String) item.getOrDefault("nAttributeValueSeq", "")).split(","));
+        String[] nAttributeValueSeqStrList = ((String) item.getOrDefault("nAttributeValueSeq", "")).split(",");
         List<Integer> nAttributeValueSeqList = new ArrayList<>();
         for (String attr : nAttributeValueSeqStrList) {
             nAttributeValueSeqList.add(Integer.parseInt(attr));
         }
 
-        Set<Map<String, Object>> mapping = new HashSet<>();
+        Set<Map<String, Object>> mapping = null;
         if (makerCode != null && brandCode != null) {
             // 찾아본다.
-            mapping = categoryScheduler.findUICategoryList(categoryCode, nAttributeValueSeqList, makerCode, brandCode);
+            for (int i = 4; i >= 1; i--) {
+                int code = Integer.parseInt(item.getOrDefault("categoryCode" + i, "0").toString());
+                if (code != 0) {
+                    try {
+                        mapping = categoryScheduler.findUICategoryList(code, nAttributeValueSeqList, makerCode, brandCode);
+                        if (mapping.size() > 0) {
+                            break;
+                        }
+                    } catch (Exception e) {
+                        logger.error("{}   {}   {}   {} ", code, nAttributeValueSeqList, makerCode, brandCode);
+                        logger.error("", e);
+                    }
+
+                }
+            }
         }
 
         /*  구조
