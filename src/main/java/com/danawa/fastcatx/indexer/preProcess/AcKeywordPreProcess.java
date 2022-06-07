4a77aa2709d83568e8afeb8f735ad43916dac1f1
ysban <covj12@gmail.com>
2022-06-07 16:06:58 +0900

    제한 키워드 파일로딩 로그 추가

--- src/main/java/com/danawa/fastcatx/indexer/preProcess/AcKeywordPreProcess.java
+++ src/main/java/com/danawa/fastcatx/indexer/preProcess/AcKeywordPreProcess.java
@@ -705,6 +705,7 @@ public class AcKeywordPreProcess implements PreProcess {
             List<String> tempList = new ArrayList<>();
             Path path = Paths.get(exceptKeywordPath + exceptTarget + ".txt");
             try(final Stream<String> excludeLines = Files.lines(path)){
+                logger.info("{} Loaded", path);
                 Iterator<String> itr = excludeLines.iterator();
                 while( itr.hasNext() ) {
                     tempList.add(itr.next());
